bcf1d5414d521a0795e43767a87bcfdfc6a6cdc1
Percival-Ashley <percivalashley1@gmail.com>
2025-06-11 19:05:48 +0300

    Update JS
    Created inner HTML for the jokeElement to say a phrase. So whenever we click on the button, it goes inside a function called generateJoke that prevents the default behavior, selects the jokeElement with th ID "joke"  and created an innerHTML which says the phrase.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function generateJoke(event) {
   event.preventDefault();
 
   let jokeElement = document.getElementById("#joke");
+  jokeElement.innerHTML = "Generating joke for you...";
 }
 
 let generatorButton = document.getElementById("generator-joke-button");

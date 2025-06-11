a621b458a9846de9c76dec45e03a40013577331d
Percival-Ashley <percivalashley1@gmail.com>
2025-06-11 19:42:06 +0300

    Update JS and HTML
    Created variables, giving them context and replaced them in the API URL. Also replaced key with my API key.
    
    Imported axios to get the url and display joke  and created a new function displayJoke that will get a response and display the joke.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+function displayJoke(response) {}
+
 function generateJoke(event) {
   event.preventDefault();
 
   let apiKey = "3471d6bbaf0b6da9tcfad5o5ae1dc50c";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
+  let context =
+    "You are a funny AI Assistant that tells short and sweet jokes.";
+  let prompt = "Generate a short and funny joke.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.ger(apiUrl).then(displayJoke);
 }
 let generatorButton = document.getElementById("generator-joke-button");
 generatorButton.addEventListener("click", generateJoke);

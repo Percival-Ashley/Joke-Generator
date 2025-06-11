eb80c45fa22b041de76f2988f2490b4c95e21064
Percival-Ashley <percivalashley1@gmail.com>
2025-06-11 19:21:26 +0300

    Update JS
    I want to use AI to generate a short joke so I decided to call an API  and inside the callback, insert a joke. I also want to typewrite it. So I added my API key and API URL.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function generateJoke(event) {
   event.preventDefault();
 
-  let jokeElement = document.getElementById("#joke");
-  jokeElement.innerHTML = "Generating joke for you...";
+  let apiKey = "3471d6bbaf0b6da9tcfad5o5ae1dc50c";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
 }
-
 let generatorButton = document.getElementById("generator-joke-button");
 generatorButton.addEventListener("click", generateJoke);
